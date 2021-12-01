cdddad6b24773c8cd96301cf1f9acf5df0c6a543
Habib Ullah <habib@umich.edu>
2021-12-01 02:32:02 +0000

    WebsiteTest

--- runscript.sh
+++ runscript.sh
@@ -3,3 +3,4 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 rm go1.17.3.linux-amd64.tar.gz
+go run main.go -i posts -u blog
