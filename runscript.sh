5f0c50cb88472cfbf37c8bf088e743f2b4c5caaf
Serva3 <73985205+Serva3@users.noreply.github.com>
2021-12-01 13:43:00 -0500

    ScriptFile

--- runscript.sh
+++ runscript.sh
@@ -1,6 +1,1 @@
-wget https://go.dev/dl/go1.17.3.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
-rm go1.17.3.linux-amd64.tar.gz
 go run main.go -i posts -u blog
